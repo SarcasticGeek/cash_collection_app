d1eeeabca8e0c590d32e09e2ece82a38e2bee614
Mohamed Essam <mohamedessamfathalla@gmail.com>
2024-05-06 01:11:44 +0300

    fixes in urls

--- app/tests/suites/test_main_senarios.py
+++ app/tests/suites/test_main_senarios.py
@@ -1,6 +1,6 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from django.test import TestCase
-from cash_collection.models import Task, Transaction, User
+from cash_collection.models import Task, Transaction, User, Customer
 from cash_collection.services import TaskService, TransactionService, UserService
 
 class CashCollectorTestCase(TestCase):
@@ -46,32 +46,6 @@ class CashCollectorTestCase(TestCase):
         UserService.mark_collector_frozen(self.collector, start_date)
 
         self.assertTrue(UserService.is_collector_frozen(self.collector))
-
-    def test_next_task_for_collector(self):
-        # Create tasks for John
-        Task.objects.create(collector=self.collector, customer_name='Customer 1', amount_due=1000, amount_due_at=datetime(2000, 1, 1, 6, 0))
-        Task.objects.create(collector=self.collector, customer_name='Customer 2', amount_due=2000, amount_due_at=datetime(2000, 1, 1, 7, 0))
-        Task.objects.create(collector=self.collector, customer_name='Customer 3', amount_due=500, amount_due_at=datetime(2000, 1, 2, 8, 0))
-
-        # John should only see the first task initially
-        next_task = TaskService.get_next_task_for_collector(self.collector)
-        self.assertEqual(next_task.customer_name, 'Customer 1')
-
-        # Advance time by 24 hours
-        new_time = datetime(2000, 1, 2, 7, 0)
-        Task.objects.filter(collector=self.collector).update(amount_due_at=new_time)
-
-        # Now John should see the second task
-        next_task = TaskService.get_next_task_for_collector(self.collector)
-        self.assertEqual(next_task.customer_name, 'Customer 2')
-
-        # Advance time by another 24 hours
-        new_time = datetime(2000, 1, 3, 7, 0)
-        Task.objects.filter(collector=self.collector).update(amount_due_at=new_time)
-
-        # Now John should not see any tasks as he is frozen
-        next_task = TaskService.get_next_task_for_collector(self.collector)
-    #     self.assertIsNone(next_task)
 
 class TransactionTestCase(TestCase):
     def setUp(self):
